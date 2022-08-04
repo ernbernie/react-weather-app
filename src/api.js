8f7264c9f5cd6f12c6fbb2475ad0ba7cfe643aa8
Jesus <ernbernie420@gmail.com>
2022-08-04 15:37:06 -0700

    react weather app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "74b7ecf7e6msh38083ff3bebcfebp101818jsn8461bf5ea0a1",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "18e314856c1cb4169c275ad0a8ce4e65";
